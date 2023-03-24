5d0cc5f3e9163e6368142eebd3e5afdda903ca00
Reammaer <endshift6@gmail.com>
2023-03-25 01:42:55 +0300

    Add  GOto REPETITION operations

--- tb/tb_2.sv
+++ tb/tb_2.sv
@@ -34,6 +34,7 @@ module tb_2;
 
     // *********** Assertions ************* //
     
+    // *** THROUGHTOUT *** //
     // Three consecutive enabled occurrences of 
     // "read" followed by four enabled occurrences
     // of "write". The occurrences of "read" and
@@ -64,13 +65,83 @@ module tb_2;
         en throughout read[*3] |=> write[*4];
     endproperty: en_read_write_throughout
     EN_READ_WRITE_THROUGHTOUT: assert property(en_read_write_throughout) else begin
-        `uvm_error("A1_RAISE", $sformatf("Assertion is failing in time=%0tns", $time))
+        `uvm_error("EN_READ_WRITE_THROUGHTOUT", $sformatf("Assertion is failing in time=%0tns", $time))
     end
     EN_READ_WRITE_THROUGHTOUT_CV: cover property(en_read_write_throughout);
 
+    // *** GOto REPETITION **
+    // After request "req1" is serviced by "done" asserted, signa; "ready" should be asaserted
+    // We need to check "ready" in the clock tick following the clock tock
+    // when "done" became high for the first time after "req1" is asserted
+    bit req1;
+    bit done;
+    bit ready;
+    initial begin
+        #1000;
+        req1 = 1;
+        #400;
+        done = 1;
+        @($global_clock);
+        ready = 1;
+    end
 
+    // property req_done_ready;
+    //     req1 ##1 !done[*] ##1 done |=> ready;
+    // endproperty: req_done_ready
+                        
+                        // ***** OR ***** //
+    property req_done_ready;
+        req1 ##1 done[->1] |=> ready;
+    endproperty: req_done_ready
+    
+    REQ_DONE_READY: assert property(req_done_ready) else begin
+        `uvm_error("REQ_DONE_READY", $sformatf("Assertion is failing in time=%0tns", $time))
+    end
+    REQ_DONE_READY_CV: cover property(req_done_ready);
 
+    // After "start2" is asserted, at each occurence of request "req2",
+    // starting from the fourth end ending with the fifth one,
+    // enable "en2" must be asserted
+    task wait_n_cycles(int n);
+        repeat(n) @($global_clock);
+    endtask: wait_n_cycles
 
+    // bit req2, start2, en2;
+    // initial begin
+    //     #500;
+    //     start2 = 1;
+    //     wait_n_cycles(3);
+    //     req2 = 1;
+    //     en2 = 1;
+    //     start2 = 0;
+    //     wait_n_cycles(1);
+    //     req2 = 0;
+    //     en2 = 0;
+    // end
 
+    // property start_req_en;
+    //     start2 ##1 req2[->4:5] |-> en2;
+    // endproperty: start_req_en
+    // START_REQ_EN: assert property(start_req_en) else begin
+    //     `uvm_error("START_REQ_EN", $sformatf("Assertion is failing in time=%0tns", $time))
+    // end
+    // START_REQ_EN_CV: cover property(start_req_en);
+
+    // After "start_ev", signal "next" should be asserted at least ten times;
+    bit start_ev, next;
+    initial begin
+        #500;
+        start_ev = 1;
+        wait_n_cycles(1);
+        next = 1;
+        wait_n_cycles(1);
+        start_ev = 0;
+        wait_n_cycles(10);
+        next = 0;
+        
+    end
+
+    START_ENV:      assert property(start_ev |=> strong(next[->10]));  
+    START_ENV_CV:   cover property(start_ev |=> strong(next[->10]));
 
 endmodule: tb_2
--- tcl/script_2.tcl
+++ tcl/script_2.tcl
@@ -14,8 +14,19 @@ add wave -divider "Top-level signals"
 add wave -radix unsigned tb_2/*
 
 ## Asseerts
+add wave -divider "THROUGHTOUT"
 add wave tb_2/EN_READ_WRITE_THROUGHTOUT
 add wave tb_2/EN_READ_WRITE_THROUGHTOUT_CV
+
+add wave -divider "GOTO 1"
+add wave tb_2/REQ_DONE_READY
+add wave tb_2/REQ_DONE_READY_CV
+
+add wave -divider "GOTO 2"
+# add wave tb_2/START_REQ_EN
+# add wave tb_2/START_REQ_EN_CV
+add wave tb_2/START_ENV
+add wave tb_2/START_ENV_CV
 
 
 run 3us
